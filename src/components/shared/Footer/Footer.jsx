73bd7fd55f0731681f7157fb8fd78057ba5bf73c
SymuNadeem <msymunnadeem@gmail.com>
2024-02-01 10:30:40 +0600

    social links

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,3 +1,7 @@
+import { socialLinks } from "@/data/footer";
+import Image from "next/image";
+import Link from "next/link";
+import flogo from "../../../assets/footer_logo.png";
 import FooterTop from "./FooterTop";
 
 const Footer = () => {
@@ -6,8 +10,20 @@ const Footer = () => {
        <footer className="bg-white">
           <FooterTop />
            <div className="container_fluid">
-             <div className="pt-[59px] pb-[62px] grid grid-cols-6 gap-8">
-                    
+             <div className="pt-[59px] pb-[62px] grid grid-cols-6 ">
+                  <div className="col-span-2 ">
+                        <div className="w-[176px] h-[176px]">
+                          <Image src={flogo} alt="footer-logo" className="w-full h-full object-fill" />
+                        </div>
+                        <p className="max-w-[390px] text-lg leading-7 text-blackPrimary/80">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="flex items-center gap-5 pt-6">
+                            {socialLinks.map((socialLink,index)=>(
+                              <Link key={index} href={socialLink.href}>
+                                  <Image src={socialLink.icon} alt="" />
+                              </Link>
+                            ))}
+                        </div>
+                  </div>  
              </div>
            </div>
        </footer>
---
+++ src/data/footer.js
@@ -0,0 +1,50 @@
+import facebook from "../assets/icons/facebook.svg";
+import instra from "../assets/icons/instagram.svg";
+import linkend from "../assets/icons/linkend.svg";
+import twiter from "../assets/icons/twiter.svg";
+
+export const socialLinks = [
+  {
+    id:1,
+    icon: facebook,
+    href: "#"
+  },
+  {
+    id:2,
+    icon: twiter,
+    href: "#"
+  },
+  {
+    id:3,
+    icon: linkend,
+    href: "#"
+  },
+  {
+    id:4,
+    icon: instra,
+    href: "#"
+  },
+  
+];
+
+export const quickLink = [
+  {
+    title: "QUICK LINKS",
+    links: [
+      { label: "about us", href: "#" },
+      { label: "contact us", href: "#" },
+      { label: "products", href: "#" },
+      { label: "login", href: "#" },
+      { label: "sign up", href: "#" },
+    ],
+  },
+  {
+    title: "CUSTOMER AREA",
+    links: [
+      { label: "my account", href: "#" },
+      { label: "orders", href: "#" },
+      { label: "terms", href: "#" },
+      { label: "shipping information", href: "#" },
+    ],
+  }
+];
