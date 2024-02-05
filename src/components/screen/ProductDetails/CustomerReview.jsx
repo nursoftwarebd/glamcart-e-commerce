9a574bccb88b6339e26182d64b69fa8c72a08d71
SymuNadeem <msymunnadeem@gmail.com>
2024-02-05 16:00:33 +0600

    review dynamic from customer data

--- src/components/screen/ProductDetails/CustomerReview.jsx
+++ src/components/screen/ProductDetails/CustomerReview.jsx
@@ -1,11 +1,12 @@
 "use client";
+import { customer } from "@/data/customer";
 import Image from "next/image";
 import five_star from "../../../assets/icons/five_star.svg";
 import Progressbar from "./Progressbar";
 
 const CustomerReview = () => {
   return (
-    <div className="mt-7.5  bg-white border border-red-600">
+    <div className="mt-7.5  bg-white border border-border">
       <div className="pl-14 pt-10 pb-9 flex items-center gap-[74px]">
         <div className="space-y-5">
           <h5 className="text-black3D font-medium text-xl leading-[30px]">
@@ -21,6 +22,43 @@ const CustomerReview = () => {
         <button className="w-[200px] h-10 rounded-[3px] bg-secondary opacity-90 shadow-3xl text-white text-sm font-medium leading-5">
           Write a Review
         </button>
+      </div>
+      <div className="">
+        <h4 className="pl-14 pb-5 border-b-[2px] border-border/70 text-2xl leading-7 font-medium text-blackPrimary">
+          Reviews (4)
+        </h4>
+        <div className="px-14 pt-5 space-y-9">
+          {customer.map((items, index) => (
+            <div key={index} className="space-y-6">
+              <div className="flex gap-4">
+                <div className="w-14 h-14">
+                  <Image
+                    src={items.image}
+                    alt={items.name}
+                    className="w-full h-full rounded"
+                  />
+                </div>
+                <div className="flex flex-col justify-between">
+                  <p className="text-lg leading-[27px] font-medium text-blackSec">
+                    {items.name}
+                  </p>
+                  <div className="flex items-center gap-4">
+                    <span className="text-graRate font-medium text-lg leading-[27px]">
+                      {items.rate}
+                    </span>
+                    <Image src={items.star} alt="star" />
+                    <p className="text-oldGray text-lg leading-[27px]">
+                      {items.time}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-blackSec text-lg leading-[27px]">
+                {items.description}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/data/customer.js
@@ -0,0 +1,48 @@
+import star from "../assets/icons/all_star_rating.svg";
+import customer1 from "../assets/images/productDetails/customer1.png";
+import customer2 from "../assets/images/productDetails/customer2.png";
+import customer3 from "../assets/images/productDetails/customer3.png";
+import customer4 from "../assets/images/productDetails/customer4.png";
+
+export const customer = [
+  {
+    id: 1,
+    name: "Vanille",
+    image: customer1,
+    rate: "5.0",
+    time: "1 Month Ago",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    star: star,
+  },
+  {
+    id: 2,
+    name: "Maduin",
+    image: customer2,
+    rate: "5.0",
+    time: "1 Month Ago",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    star: star,
+  },
+  {
+    id: 3,
+    name: "Samantha",
+    image: customer3,
+    rate: "5.0",
+    time: "1 Month Ago",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    star: star,
+  },
+  {
+    id: 4,
+    name: "Roveria",
+    image: customer4,
+    rate: "5.0",
+    time: "1 Month Ago",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    star: star,
+  },
+];
