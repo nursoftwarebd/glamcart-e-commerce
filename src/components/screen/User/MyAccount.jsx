3aa37c15577fb1ddf277b7b7d5b33bf0bb1cc398
SymuNadeem <msymunnadeem@gmail.com>
2024-02-08 11:48:49 +0600

    my order done

--- src/components/screen/User/MyAccount.jsx
+++ src/components/screen/User/MyAccount.jsx
@@ -5,7 +5,7 @@ import edit from "../../../assets/icons/edit.svg";
 const MyAccount = () => {
   return (
     <div>
-      <div className="bg-white">
+      <div className="bg-white shadow-md">
         <div className="py-3 px-5 border-borderAccount border-b-[.75px] flex items-center justify-between">
           <h4 className="font-rajdhani text-accountBlack text-xl leading-6 tracking-[2%] font-semibold">
             Personal Information
@@ -52,7 +52,7 @@ const MyAccount = () => {
           ))}
         </div>
       </div>
-      <div className="mt-5 bg-white">
+      <div className="mt-5 bg-white shadow-md">
         <div className="py-3 px-5 border-borderAccount border-b-[.75px] flex items-center justify-between">
           <p className="font-rajdhani text-accountBlack text-xl leading-6 tracking-[2%] font-semibold uppercase">
             ADDRESS BOOK
--- src/components/screen/User/MyOrder.jsx
+++ src/components/screen/User/MyOrder.jsx
@@ -1,32 +1,35 @@
 import { myOrder } from "@/data/myorder";
+import Link from "next/link";
 
 const MyOrder = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-white shadow-md">
       <div className="py-3 px-5 border-borderAccount border-b-[.75px] flex items-center justify-between">
         <h4 className="font-rajdhani text-accountBlack text-xl leading-6 tracking-[2%] font-semibold">
           My Order
         </h4>
       </div>
-      <div className="px-5 py-6.5">
+      <div className="px-5 pt-6.5 pb-13">
         <table className="w-full ">
-          <thead className="border ">
+          <thead className="border-[.75px] border-borderAccount bg-gradient-to-r from-[#E6E7EC]/[32.5%] via-[#E9EAED]/[24%] to-[#E7E9EE]/[28.5%]">
             <tr>
-              <th className="text-start border">Order #</th>
-              <th className="text-start border">Date</th>
-              <th className="text-start border">Order amount</th>
-              <th className="text-start border">status</th>
-              <th className="text-start border">Actions</th>
+              <th className="order_th">Order #</th>
+              <th className="order_th">Date</th>
+              <th className="order_th">Order amount</th>
+              <th className="order_th">status</th>
+              <th className="order_th">Actions</th>
             </tr>
           </thead>
           <tbody>
             {myOrder.map((item, index) => (
               <tr key={index}>
-                <td>{item.order}</td>
-                <td>{item.date}</td>
-                <td>{item.amount}</td>
-                <td>{item.status}</td>
-                <td>{item.action}</td>
+                <td className="order_td">{item.order}</td>
+                <td className="order_td">{item.date}</td>
+                <td className="order_td">{item.amount}</td>
+                <td className="order_td">{item.status}</td>
+                <td className="order_td">
+                  <Link href={"#"}>{item.action}</Link>
+                </td>
               </tr>
             ))}
           </tbody>
