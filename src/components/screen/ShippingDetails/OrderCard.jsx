cc39ab4a98e9592fe8597a650037cbed8b60a7ed
SymuNadeem <msymunnadeem@gmail.com>
2024-02-10 21:40:36 +0600

    card for shipping

--- src/components/screen/ShippingDetails/OrderCard.jsx
+++ src/components/screen/ShippingDetails/OrderCard.jsx
@@ -1,10 +1,75 @@
+import RoundedCheckbox from "@/components/common/RoundedCheckbox";
+
 const OrderCard = () => {
   return (
-    <div className="w-auto border border-red-500">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos perferendis
-      vitae iste eius vero. Alias ullam doloribus ad laudantium accusamus
-      voluptas eos quidem consequuntur dolore enim, placeat velit reiciendis
-      dicta?
+    <div className="w-full xl:w-[439px] 2xl:w-[439px] border border-red-500 p-7 bg-white rounded-[5px]">
+      <h5 className="text-xl text-blackPrimary font-medium leading-[30px]">
+        My Orders
+      </h5>
+      <div className="py-7.5 border-b-[1.73px] border-lightGrayBlue space-y-3">
+        <div className="flex items-center justify-between">
+          <p className="max-w-[203px] text-lg text-lineThrough leading-[26px]">
+            LED Monitor With High Quality In The World
+          </p>
+          <div>
+            <h5 className="flex items-center text-xl  leading-[30px] text-lineThrough gap-3">
+              <span>1X</span>
+              <span className="font-medium text-blackPrimary">৳976.33</span>
+            </h5>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="max-w-[203px] text-lg text-lineThrough leading-[26px]">
+            Magic Pen for iPad
+          </p>
+          <div>
+            <h5 className="flex items-center text-xl  leading-[30px] text-lineThrough gap-3">
+              <span>1X</span>
+              <span className="font-medium text-blackPrimary">৳976.33</span>
+            </h5>
+          </div>
+        </div>
+      </div>
+      <div className="pt-7 pb-5 border-b-[1.73px] border-lightGrayBlue space-y-3">
+        <div className="flex items-center justify-between">
+          <p className="max-w-[203px] text-lg text-lineThrough leading-[26px]">
+            Subtotal
+          </p>
+          <div>
+            <h5 className="flex items-center text-xl  leading-[30px] text-lineThrough gap-3">
+              <span>1X</span>
+              <span className="font-medium text-blackPrimary">৳968.56</span>
+            </h5>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="max-w-[203px] text-lg text-lineThrough leading-[26px]">
+            Free Shipping
+          </p>
+          <div>
+            <h5 className="flex items-center text-xl  leading-[30px] text-lineThrough gap-3">
+              <span>1X</span>
+              <span className="font-medium text-blackPrimary">৳0</span>
+            </h5>
+          </div>
+        </div>
+      </div>
+      <div className="py-7.5 border-b-[1.73px] border-lightGrayBlue flex items-center justify-between">
+        <h5 className="text-lg text-blackPrimary font-medium leading-[27px]">
+          Order Total
+        </h5>
+        <h3 className="text-heading28 text-secondary font-semibold leading-[42px]">
+          ৳1,956.66
+        </h3>
+      </div>
+      <div className="py-7">
+        <p className="p-6">Payment Methods</p>
+        <div className="space-y-4">
+          <div className="flex items-center justify-between">
+            <RoundedCheckbox />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/screen/ShippingDetails/ShippingForm.jsx
+++ src/components/screen/ShippingDetails/ShippingForm.jsx
@@ -2,7 +2,7 @@ import Checkbox from "@/components/common/Checkbox";
 
 const ShippingForm = () => {
   return (
-    <div className="w-[920px] border border-red-500">
+    <div className="w-full  2xl:w-[920px] border border-red-500">
       <div className="bg-white shadow-md">
         <div className="py-4 px-[38px] border-borderAccount border-b-[.75px] flex items-center justify-between">
           <h3 className="font-rajdhani text-accountBlack text-heading30 leading-6 tracking-[2%] font-semibold">
@@ -15,42 +15,42 @@ const ShippingForm = () => {
               <h5 className="form_title">Contact Information</h5>
               <div className="space-y-5">
                 <div className="w-full flex items-center gap-10">
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       First name
                     </label>
                     <input
                       type="text"
-                      className="w-full h-[45px] xl:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
+                      className="w-full h-[45px] lg:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
                     />
                   </div>
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       Last name
                     </label>
                     <input
                       type="text"
-                      className="w-full h-[45px] xl:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
+                      className="w-full h-[45px] lg:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
                     />
                   </div>
                 </div>
                 <div className="w-full flex items-center gap-10">
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       EMAIL ADDRESS
                     </label>
                     <input
                       type="email"
-                      className="w-full h-[45px] xl:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
+                      className="w-full h-[45px] lg:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
                     />
                   </div>
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2  space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       MOBILE NUMBER <span className="text-red-500">*</span>
                     </label>
                     <input
                       type="text"
-                      className="w-full h-[45px] xl:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
+                      className="w-full h-[45px] lg:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
                     />
                   </div>
                 </div>
@@ -80,7 +80,7 @@ const ShippingForm = () => {
               </div>
               <div className="space-y-5">
                 <div className="w-full flex items-center gap-10">
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       Country <span className="text-red-500">*</span>
                     </label>
@@ -89,7 +89,7 @@ const ShippingForm = () => {
                       className="w-full h-[45px] xl:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
                     />
                   </div>
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       City/Area <span className="text-red-500">*</span>
                     </label>
@@ -100,7 +100,7 @@ const ShippingForm = () => {
                   </div>
                 </div>
                 <div className="w-full flex items-center gap-10">
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       Select at district, state of province
                     </label>
@@ -109,7 +109,7 @@ const ShippingForm = () => {
                       className="w-full h-[45px] xl:w-[400px] px-4 py-3 outline-none bg-backgroundPage rounded-[2px] border-[1px] border-grayBorderForm"
                     />
                   </div>
-                  <div className="space-y-3 flex flex-col">
+                  <div className="w-1/2 space-y-3 flex flex-col">
                     <label htmlFor="" className="address_form_label">
                       Postal Code <span className="text-red-500">*</span>
                     </label>
