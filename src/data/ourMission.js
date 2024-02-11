7b63954783dc5d7070802c84f4d3d9f56ffc720e
SymuNadeem <msymunnadeem@gmail.com>
2024-02-11 21:28:56 +0600

    our mission data set

---
+++ src/data/ourMission.js
@@ -0,0 +1,30 @@
+import one from "../assets/icons/mission1.svg";
+import two from "../assets/icons/mission2.svg";
+import three from "../assets/icons/mission3.svg";
+
+export const ourMission = [
+  {
+    id: 1,
+    title: "Best Online Shop ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    img: one,
+    link: "/online-shop",
+  },
+  {
+    id: 2,
+    title: "Trusted Seller",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    img: two,
+    link: "/trusted-seller",
+  },
+  {
+    id: 3,
+    title: "Expand Store",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    img: three,
+    link: "/expand-store",
+  },
+];
