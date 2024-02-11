f64671fbfb3c2a903867acbc96b2721994afce67
SymuNadeem <msymunnadeem@gmail.com>
2024-02-11 22:53:37 +0600

    our feature data create

--- src/components/screen/About/OurFeature.jsx
+++ src/components/screen/About/OurFeature.jsx
@@ -1,5 +1,17 @@
+import Title from "./Title";
+
 const OurFeature = () => {
-  return <div>OurFeature</div>;
+  return (
+    <div className="pt-16">
+      <div className="container_fluid">
+        <Title
+          heading="Feature"
+          sebHeading="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        />
+        <div className="mt-9 px-[100px] py-10 bg-white shadow-md border-[1px] grid grid-cols-4"></div>
+      </div>
+    </div>
+  );
 };
 
 export default OurFeature;
---
+++ src/data/ourFeature.js
@@ -0,0 +1,31 @@
+import customer from "../assets/icons/happy.svg";
+import products from "../assets/icons/products.svg";
+import seller from "../assets/icons/seller.svg";
+import support from "../assets/icons/support.svg";
+
+export const ourFeature = [
+  {
+    id: 1,
+    img: customer,
+    figure: "50,663",
+    category: "Happy Customers",
+  },
+  {
+    id: 2,
+    img: products,
+    figure: "10,672+",
+    category: "Our Products",
+  },
+  {
+    id: 3,
+    img: seller,
+    figure: "500+",
+    category: "Sellers",
+  },
+  {
+    id: 4,
+    img: support,
+    figure: "24/7",
+    category: "Customer Support",
+  },
+];
