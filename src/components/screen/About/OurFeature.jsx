7aa124fd864cf5b52f889537dd720e208a61f074
SymuNadeem <msymunnadeem@gmail.com>
2024-02-12 18:45:36 +0600

    price range

--- src/components/screen/About/OurFeature.jsx
+++ src/components/screen/About/OurFeature.jsx
@@ -10,7 +10,7 @@ const OurFeature = () => {
           heading="Feature"
           sebHeading="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
         />
-        <div className="mt-9 px-[100px] py-10 bg-white shadow-md border-[1px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="mt-9 sm:px-15 lg:px-[100px] py-10 bg-white shadow-md border-[1px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-7 lg:gap-y-0 ">
           {ourFeature.map((ourFeature) => {
             return (
               <div key={ourFeature.id}>
@@ -23,7 +23,7 @@ const OurFeature = () => {
                       />
                     </div>
                   </div>
-                  <div className="pt-9 space-y-2">
+                  <div className="pt-4 lg:pt-9 space-y-2">
                     <h2 className="text-2xl md:text-heading30 xl:text-heading40 font-semibold leading-[60px] text-blackPrimary">
                       {ourFeature.figure}
                     </h2>
