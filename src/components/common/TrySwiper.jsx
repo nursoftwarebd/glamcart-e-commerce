b65cd5933e7ac210319125af7efb87707f62b292
SymuNadeem <msymunnadeem@gmail.com>
2024-02-14 20:26:07 +0600

    menu dynamic

--- src/components/common/TrySwiper.jsx
+++ src/components/common/TrySwiper.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { navLinks } from "@/data/navLinks";
 import Image from "next/image";
 import Link from "next/link";
 import "swiper/css";
@@ -44,74 +45,13 @@ const TrySwiper = () => {
         modules={[Navigation]}
         className="mySwiper small_nav_text"
       >
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            WOMENS FASHION
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            MENS FASHION
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            KIDS FASHION
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            HOME & LIFESTYLE
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            ARTS & CRAFTS
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            COMPUTER & ELECTRONICS
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            FOOD & GROCERY
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            WOMENS FASHION
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            MENS FASHION
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            KIDS FASHION
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            HOME & LIFESTYLE
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            ARTS & CRAFTS
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"} className="border border-red-500">
-            COMPUTER & ELECTRONICS
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link href={"#"}> FOOD & GROCERY </Link>
-        </SwiperSlide>
+        {navLinks.map((navLinks, index) => (
+          <SwiperSlide key={index}>
+            <Link href={navLinks.href} className="border border-red-500">
+              {navLinks.name}
+            </Link>
+          </SwiperSlide>
+        ))}
       </Swiper>
 
       <button class="next-slide-button next_button">
