8b47435040a0a2c18c6051d6b2789b06559511e0
SymuNadeem <msymunnadeem@gmail.com>
2024-02-14 11:50:32 +0600

    user account small screen

--- src/app/(customer)/layout.jsx
+++ src/app/(customer)/layout.jsx
@@ -1,14 +1,49 @@
 "use client";
 import { tabs } from "@/data/tabs";
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const CustomerLayout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <section className="pt-6 pb-21 bg-backgroundPage">
       <div className="container_fluid">
-        <h3 className="my_account">My Account</h3>
+        <div className="flex items-center justify-between">
+          <h3 className="my_account">My Account</h3>
+          <div onClick={toggleMenu} className="flex lg:hidden">
+            =
+          </div>
+        </div>
         <div className="pt-6 flex flex-col lg:flex-row gap-5">
-          <div className="w-[280px] h-fit bg-white shadow-md flex flex-col">
+          <div
+            className={`lg:hidden ${
+              isOpen ? "block" : "hidden"
+            } fixed top-0 left-0 inset-0 z-50 bg-white px-4  flex  flex-col justify-center items-center shadow-md transition-all duration-300`}
+          >
+            {tabs.map((tab) => (
+              <div
+                key={tab.id}
+                className="border-dashed border-b-[1px] w-full pl-[18px] py-[14px] text-start account_list"
+              >
+                <Link
+                  href={tab.link}
+                  onClick={toggleMenu}
+                  className="flex items-center  gap-2"
+                >
+                  <Image src={tab.img} alt="" />
+                  {tab.title}
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          {/* large screen */}
+          <div className="w-[280px] h-fit bg-white shadow-md hidden lg:flex flex-col">
             {tabs.map((tab) => (
               <div
                 key={tab.id}
--- src/data/tabs.js
+++ src/data/tabs.js
@@ -1,9 +1,17 @@
+import pic5 from "../assets/icons/user-account/accInfo.svg";
+import pic4 from "../assets/icons/user-account/adbook.svg";
+import pic7 from "../assets/icons/user-account/contact-us.svg";
+import pic6 from "../assets/icons/user-account/faq.svg";
+import pic1 from "../assets/icons/user-account/my-account.svg";
+import pic2 from "../assets/icons/user-account/order.svg";
+import pic3 from "../assets/icons/user-account/wishlist.svg";
+
 export const tabs = [
-  { id: 1, title: "my account", link: "/my-account" },
-  { id: 2, title: "My Orders", link: "/my-orders" },
-  { id: 3, title: "My Wishlist", link: "/my-wishlist" },
-  { id: 4, title: "Address Book", link: "/address-book" },
-  { id: 5, title: "Account Information", link: "/account-info" },
-  { id: 6, title: "Help & FAQ", link: "/help-faq" },
-  { id: 7, title: "Contact Us", link: "/contact-us" },
+  { id: 1, title: "my account", link: "/my-account", img: pic1 },
+  { id: 2, title: "My Orders", link: "/my-orders", img: pic2 },
+  { id: 3, title: "My Wishlist", link: "/my-wishlist", img: pic3 },
+  { id: 4, title: "Address Book", link: "/address-book", img: pic4 },
+  { id: 5, title: "Account Information", link: "/account-info", img: pic5 },
+  { id: 6, title: "Help & FAQ", link: "/help-faq", img: pic6 },
+  { id: 7, title: "Contact Us", link: "/contact-us", img: pic7 },
 ];
