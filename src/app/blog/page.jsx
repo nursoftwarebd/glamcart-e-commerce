45b2c821e3218b8cc36806f97a89706a31a8cf1c
fayshal <imfayshalrana@gmail.com>
2024-03-20 03:48:07 +0600

    make blog card as a component

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,6 +1,7 @@
+import BlogCard from "@/components/screen/Blog/BlogCard";
 import BlogPaginations from "@/components/screen/Blog/BlogPaginations";
 import { blogData } from "@/data/blogData";
-import Image from "next/image";
+
 import Link from "next/link";
 
 export const metadata = {
@@ -28,38 +29,9 @@ const BlogPage = () => {
         </div>
         <Link href={"/blog-details"}>
           <div className="mt-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-12 gap-x-7 2xl:gap-x-10">
-            {blogData.map((blogData) => {
-              return (
-                <div key={blogData.id}>
-                  <div className="space-y-6">
-                    <Image
-                      src={blogData.img}
-                      alt={`blog image ${blogData.title}`}
-                      className="w-full h-full rounded-xl"
-                    />
-
-                    <div>
-                      <h3 className="text-2xl text-contactLabel leading-8 tracking-[5%] font-semibold ">
-                        Digital Payment will be Top Payment In Next 10 Years
-                      </h3>
-                      <div className="py-4 flex items-center gap-2  text-sm text-contactLabel leading-6">
-                        <p className="font-medium">{blogData.date}</p>
-                        <div className="w-1 h-1 bg-black rounded-full"></div>
-                        <p className="font-medium">Technology</p>
-                      </div>
-                      <p className="leading-[27px] text-textBody">
-                        {blogData.description}
-                      </p>
-                    </div>
-                    <div className="pt-6">
-                      <button className="w-[160px] h-[44px] flex items-center justify-center rounded-[5px] bg-secondary hover:bg-transparent hover:border-[2px] hover:border-secondary text-white hover:text-secondary duration-300">
-                        <span className="font-medium leading-6">Read More</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            {blogData.map((blogData) => 
+             <BlogCard blogData={blogData} key={blogData.id}/>
+            )}
           </div>
         </Link>
         <BlogPaginations />
